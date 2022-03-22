12b1fa8d6574b99d7c9a2228114fb0c3fdcfd28a
Lamminhtuan <tuanlam300@gmail.com>
2022-03-22 18:40:00 +0700

    Initial commit

--- main.py
+++ main.py
@@ -4,33 +4,29 @@ import time
 import pandas as pd
 dv = webdriver.Firefox()
 #Nhập id của tác giả
-id = '37268947300'
+id = '37647886800'
 dv.get('https://ieeexplore.ieee.org/author/'+id)
 #Nếu bị lỗi thì tăng giá trị time.sleep() để chờ trang load đầy đủ
-time.sleep(10)
+time.sleep(15)
 num = dv.find_element(by=By.XPATH, value='/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/xpl-author-profile/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]').text
 num = int(num)
 pages = num // 25 + 1
 items = []
 dict = {'Title': []}
-for j in range(1, pages + 1):
-    url = "https://ieeexplore.ieee.org/author/"+id+"?searchWithin=%22Author%20Ids%22:"+id+"&history=no&sortType=newest&highlight=true&returnFacets=ALL&returnType=SEARCH&pageNumber={}".format(j)
+for i in range(1, pages + 1):
+    url = "https://ieeexplore.ieee.org/author/"+id+"?searchWithin=%22Author%20Ids%22:"+id+"&history=no&sortType=newest&highlight=true&returnFacets=ALL&returnType=SEARCH&pageNumber={}".format(i)
     dv.get(url)
     # Nếu bị lỗi thì tăng giá trị time.sleep() để chờ trang load đầy đủ
-    time.sleep(10)
+    time.sleep(15)
     paper = dv.find_elements(by=By.CLASS_NAME, value='List-results-items')
-    i = 3
+    j = 3
     for item in paper:
-        xpath = """/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/main/div[2]/div[2]/xpl-results-list/div[%s]/xpl-results-item/div[1]/div[1]/div[2]/h2"""%(i)
+        xpath = """/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/main/div[2]/div[2]/xpl-results-list/div[%s]/xpl-results-item/div[1]/div[1]/div[2]/h2"""%(j)
         title = item.find_element(by=By.XPATH, value=xpath).text
         print(title)
         items.append(title)
-        i += 1
+        j += 1
 dict['Title'].extend(items)
 df = pd.DataFrame(dict)
 df.to_csv('output.csv')
-<<<<<<< HEAD
 dv.close()
-=======
-dv.close()
->>>>>>> 65a9f3f (Initial commit)
