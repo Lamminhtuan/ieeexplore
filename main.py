ecd2e0818eea9c924dcec19ac6dadfebd623ddac
Lâm Minh Tuấn <79350128+Lamminhtuan@users.noreply.github.com>
2022-03-22 18:16:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ dv.get('https://ieeexplore.ieee.org/author/'+id)
 time.sleep(10)
 num = dv.find_element(by=By.XPATH, value='/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/xpl-author-profile/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]').text
 num = int(num)
-perpage = num // 25 + 1
+pages = num // 25 + 1
 items = []
 dict = {'Title': []}
-for j in range(1, perpage+1):
+for j in range(1, pages + 1):
     url = "https://ieeexplore.ieee.org/author/"+id+"?searchWithin=%22Author%20Ids%22:"+id+"&history=no&sortType=newest&highlight=true&returnFacets=ALL&returnType=SEARCH&pageNumber={}".format(j)
     dv.get(url)
     # Nếu bị lỗi thì tăng giá trị time.sleep() để chờ trang load đầy đủ
@@ -21,7 +21,7 @@ for j in range(1, perpage+1):
     paper = dv.find_elements(by=By.CLASS_NAME, value='List-results-items')
     i = 3
     for item in paper:
-        xpath="""/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/main/div[2]/div[2]/xpl-results-list/div[%s]/xpl-results-item/div[1]/div[1]/div[2]/h2"""%(i)
+        xpath = """/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/main/div[2]/div[2]/xpl-results-list/div[%s]/xpl-results-item/div[1]/div[1]/div[2]/h2"""%(i)
         title = item.find_element(by=By.XPATH, value=xpath).text
         print(title)
         items.append(title)
