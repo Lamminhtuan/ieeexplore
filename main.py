bcf9012780508ded5b61e4f9c42d6d9dcf2efe3a
Lamminhtuan <tuanlam300@gmail.com>
2022-03-26 12:38:17 +0700

    Initial commit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ id = '37269460700'
 dv.get('https://ieeexplore.ieee.org/author/'+id)
 #Nếu bị lỗi thì tăng giá trị time.sleep() để chờ trang load đầy đủ
 time.sleep(15)
+author = dv.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/xpl-author-profile/div[1]/div[3]/div[1]/div/div[2]/h1/span[1]')
+print(author)
 num = dv.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[3]/div/xpl-root/div/xpl-search-results/xpl-author-profile/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]').text
 num = int(num)
 pages = num // 25 + 1
@@ -26,5 +28,5 @@ for i in range(1, pages + 1):
         items.append(title)
 dict['Title'].extend(items)
 df = pd.DataFrame(dict)
-df.to_csv('output.csv')
+df.to_csv(author+'.csv')
 dv.close()
